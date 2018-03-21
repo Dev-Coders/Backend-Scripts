21ddfc8c920ae7897d714699dda130924ad8bf3a
Shrey Batra <shreybatra@Shreys-MacBook-Air.local>
2018-03-21 12:14:14 +0530

    add orders and users

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, abort, make_response
 from flask_restful import Api, Resource, reqparse, fields, marshal
 from pojo import PreProcessData
+from userorder import Order
+from user import User
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,6 +13,8 @@ api = Api(app)
 #-----------FLASK----ROUTES----------
 
 api.add_resource(PreProcessData, '/preprocess', endpoint='preprocess')
+api.add_resource(Order, '/order', endpoint='order')
+api.add_resource(User, '/user', endpoint='user')
 
 
 if __name__ == '__main__':
---
+++ userorder.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+from pymongo import MongoClient
+import pymongo
+from bson import json_util
+
+'''
+Need to add authentication lateer!!
+'''
+
+
+class Order(Resource):
+
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('UserName', type=str, required=True,
+			help='No Username Provided', location='json')
+		self.reqparse.add_argument('Latitude', type=float, required=True,
+			help='Invalid Location', location='json')
+		self.reqparse.add_argument('Longitude', type=float, required=True,
+			help='Invalid Location', location='json')
+		super(Order, self).__init__()
+
+
+	def post(self):
+		client = MongoClient()
+		db = client.devcoders
+		orders = db.orders
+
+		username = request.json['UserName']
+		latitude = request.json['Latitude']
+		longitude = request.json['Longitude']
+
+		order = {
+		'UserName':username,
+		'Latitude':latitude,
+		'Longitude':longitude
+		}
+
+		orders.insert_one(order)
+
+		return {'inserted':'order'},201
+
+	def get(self):
+		client = MongoClient()
+		db = client.devcoders
+		orders = db.orders
+
+		result = orders.find()
+
+		final = []
+		for i in result:
+			i.pop('_id')
+			final.append(i)
+
+		return {'Orders':final},200
+
+	def delete(self):
+		client = MongoClient()
+		db = client.devcoders
+		orders = db.orders
+		orders.remove()
+		return {'deleted':'all'},200
+
+
